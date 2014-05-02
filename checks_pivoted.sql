bbec364b05c1e0b093d4de206616158e4ddfa361
Yuliya Zakharenko <rockroada@gmail.com>
2014-05-02 12:21:56 -0700

    checks pivoted with column names sorted and column names are in MM/yyyy format

--- checks_pivoted.sql
+++ checks_pivoted.sql
@@ -1,19 +1,26 @@
 DECLARE @cols AS NVARCHAR(MAX)
 DECLARE @query AS NVARCHAR(MAX)
-SELECT @cols = STUFF((SELECT DISTINCT  ','+
-						DATENAME(m,DATEADD(m, 1, Paydate)) +'_' + CONVERT(NVARCHAR, YEAR(DATEADD(m, 1, Paydate)))
+DECLARE @StartDate AS DATE
+DECLARE @EndDate AS DATE
+DECLARE @VendID AS NVARCHAR(10)
+SELECT @cols = STUFF((SELECT ','+
+						QUOTENAME(FORMAT(DATEADD(m, 1, Paydate), 'MM/yyyy'))
 						FROM EHIP_Checks_New
-						WHERE PayDate BETWEEN  '01-01-2014' AND '04-30-2014'
+						WHERE PayDate BETWEEN  '01-01-2014' AND DATEADD(m,1,GETDATE())
 						AND VendID = 'EXCH02'
+						GROUP BY FORMAT(DATEADD(m, 1, Paydate), 'MM/yyyy')
+						ORDER BY MAX(PayDate)
 						FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)'), 1, 1, '');
-
-SELECT @query	='SELECT * FROM (SELECT User5, ClearDate,  DATENAME(m,DATEADD(m, 1, Paydate)) +''_'' + CONVERT(NVARCHAR, YEAR(DATEADD(m, 1, Paydate))) AS Month
+						
+SELECT @query	='SELECT * FROM (SELECT User5, ClearDate,  FORMAT(DATEADD(m, 1, Paydate), ''MM/yyyy'') AS Paydate
 FROM EHIP_Checks_New WHERE PayDate BETWEEN  ''01-01-2014'' AND ''04-30-2014''
 AND VendID = ''EXCH02''
 ) AS SourceTable PIVOT
 (
-MAX(ClearDate) FOR Month IN ('+ @cols+')
+MAX(ClearDate) FOR Paydate IN ('+ @cols+')
 ) 
 AS PivotTable;';
 execute(@query);
+
+
 
